fbbced826ea0e608a28c6431fa2773fbb738012a
Olivier Perrin <34828790+olperri@users.noreply.github.com>
2020-05-09 00:37:06 +0200

    create baseclass for linear regression and add gradientdescent

--- models/linear_model.py
+++ models/linear_model.py
@@ -1,9 +1,20 @@
 
 import numpy as np
+import warnings
+warnings.simplefilter("always", UserWarning)
 
-class LinearRegression:
+class BaseLinearRegression:
+    def add_bias(self, X:np.ndarray) -> np.ndarray:
+        bias = np.ones((X.shape[0],1))
+        return np.hstack([bias,X])
+
+    def predict(self, X:np.ndarray) -> np.ndarray:
+        X = self.add_bias(X)
+        return X.dot(self.weights)
+
+class ClosedFormLinearRegression(BaseLinearRegression):
     """
-    A basic linear regression.
+    A basic linear regression suing closed form
 
     Parameters
     ----------
@@ -17,12 +28,7 @@ class LinearRegression:
     def __init__(self, method:str='pseudoinverse'):
         if method not in ['pseudoinverse', 'inverse']:
             raise NotImplementedError
-
         self.method = method
-
-    def add_bias(self, X:np.ndarray) -> np.ndarray:
-        bias = np.ones((X.shape[0],1))
-        return np.hstack([bias,X])
 
     def fit(self, X:np.ndarray, y:np.ndarray):
         X = self.add_bias(X)
@@ -33,6 +39,43 @@ class LinearRegression:
         elif self.method == 'inverse':
             self.weights = np.linalg.inv(X.T.dot(X)).dot(X.T).dot(y)
 
-    def predict(self, X:np.ndarray) -> np.ndarray:
+class GradientDescentLinearRegression(BaseLinearRegression):
+    """
+    A linear regression using gradient-descent
+
+    Parameters
+    ----------
+    method : currently only batch is supported. loss is assumed to be MSE for now
+    learning_rate : constant learning rate applied
+    tolerance : stopping tolerance applied to the model
+    max_iters : max number of iterations during the gradient descent
+
+    Attributes
+    ----------
+    weights : 1d np.ndarray containing the weights of the regression. The first one is the bias
+    """
+
+    def __init__(self, method:str='batch', learning_rate:float=0.1, tolerance:float=1e-3, max_iterations:int=10000):
+        if method not in ['batch']:
+            raise NotImplementedError
+        self.method = method
+        self.learning_rate = learning_rate
+        self.tolerance = tolerance
+        self.max_iterations = max_iterations
+
+    def fit(self, X:np.ndarray, y:np.ndarray):
         X = self.add_bias(X)
-        return X.dot(self.weights)
+
+        if self.method == 'batch':
+            # random initialization
+            self.weights = np.random.default_rng().standard_normal(size=X.shape[1])
+            has_converged = False
+            for i in range(self.max_iterations):
+                gradient = 2/X.shape[0] * X.T.dot(X.dot(self.weights)-y)
+                self.weights = self.weights - self.learning_rate * gradient
+                norm_gradient = np.linalg.norm(gradient)
+                if  norm_gradient < self.tolerance:
+                    has_converged = True
+                    break
+            if not has_converged:
+                warnings.warn("Max numbers of iterations has been reached")
